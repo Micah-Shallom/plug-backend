e21714f2ca05d10523bb5f0d144dfc8f36f0931a
Micah <micahshallom@gmail.com>
2024-03-13 12:55:42 +0100

    error handling in productRoute, categoryRoute

--- app/listings/categoryRoute.py
+++ app/listings/categoryRoute.py
@@ -8,70 +8,86 @@ from app.models.productModel import Product
 # Get products by category
 @category_bp.get("/<string:category_id>/products")
 def get_products_by_category(category_id):
-    # Query products by category_id
-    products = Product.query.filter_by(category_id=category_id).all()
+    try:
+        # Query products by category_id
+        products = Product.query.filter_by(category_id=category_id).all()
 
-    # Convert products to JSON format
-    products_data = []
+        if not products:
+            # Return 404 if no products found for the category
+            return jsonify({"message": "No products found for the category"}), 404
 
-    for product in products:
-        product_data = {
-            'id': product.product_id,
-            'title': product.title,
-            'description': product.description,
-            'price': product.price,
-            'category_id': product.category_id
-        }
-        products_data.append(product_data)
+        # Convert products to JSON format
+        products_data = []
+
+        for product in products:
+            product_data = {
+                'id': product.product_id,
+                'title': product.title,
+                'description': product.description,
+                'price': product.price,
+                'category_id': product.category_id
+            }
+            products_data.append(product_data)
+        
+        return jsonify({
+            "message": "Category Products fetched successfully",
+            "results": products_data
+        }), 200
+
+    except Exception as e:
+        # Handle unexpected errors
+        return jsonify({"message": str(e)}), 500
     
-    return jsonify({
-        "message": "Category Products fetched successfully",
-        "results": products_data
-    }), 201
 
 # Route to retrieve a paginated list of products based on a specific category
 @category_bp.get("/<string:category_id>/paginated-products")
 def get_paginated_products_by_category(category_id):
     # Pagination parameters
-    page = request.args.get("page", 1, type=int)
-    per_page = request.args.get("per_page", 10, type=int)
 
-    # Query category by category_id
-    category = Category.query.get(category_id)
+    try:
 
-    if not category:
-        return jsonify({"error": "Category not found"}), 404
-    
-    # Query products with pagination
-    products = Product.query.filter_by(category_id=category_id).paginate(
-        page=page,
-        per_page=per_page
-    )
+        page = request.args.get("page", 1, type=int)
+        per_page = request.args.get("per_page", 10, type=int)
 
-    # Convert products to JSON format
-    products_data = []
+        # Query category by category_id
+        category = Category.query.get(category_id)
 
-    for product in products.items:
-        product_data = {
-            'id': product.product_id,
-            'title': product.title,
-            'description': product.description,
-            'price': product.price,
-            'category_id': product.category_id
+        if not category:
+            return jsonify({"error": "Category not found"}), 404
+        
+        # Query products with pagination
+        products = Product.query.filter_by(category_id=category_id).paginate(
+            page=page,
+            per_page=per_page
+        )
+
+        # Convert products to JSON format
+        products_data = []
+
+        for product in products.items:
+            product_data = {
+                'id': product.product_id,
+                'title': product.title,
+                'description': product.description,
+                'price': product.price,
+                'category_id': product.category_id
+            }
+            products_data.append(product_data)
+
+        # Construct pagination data
+        pagination_data = {
+            'total': products.total,
+            'pages': products.pages,
+            'page': products.page,
+            'per_page': products.per_page,
+            'has_next': products.has_next,
+            'has_prev': products.has_prev
         }
-        products_data.append(product_data)
 
-    # Construct pagination data
-    pagination_data = {
-        'total': products.total,
-        'pages': products.pages,
-        'page': products.page,
-        'per_page': products.per_page,
-        'has_next': products.has_next,
-        'has_prev': products.has_prev
-    }
-
-    return jsonify({"category": category.name, "products": products_data, "pagination": pagination_data})
+        return jsonify({"category": category.name, "products": products_data, "pagination": pagination_data})
+    
+    except Exception as e:
+        return jsonify({"message": str(e)}), 500
 
 # Create a new category
 @category_bp.post("/create")
@@ -99,14 +115,17 @@ def create_category():
 @category_bp.get("/all")
 def get_all_categories():
     # Query all categories
-    data = Category.query.all()
+    try:
+        data = Category.query.all()
 
-    category_list = []
+        category_list = []
 
-    for cat in data:
-        category_list.append({
-            "name": cat.name,
-            "description": cat.description
-        })
+        for cat in data:
+            category_list.append({
+                "name": cat.name,
+                "description": cat.description
+            })
 
-    return jsonify(category_list), 200
+        return jsonify(category_list), 200
+    except Exception as e:
+        return jsonify({"message": str(e)}), 500
